0c30d7f867e5c8b041892eb23ddb701f27bd2aed
ngarcia-dev <garcianahuel77@gmail.com>
2024-10-25 21:37:59 -0300

    add: view details product

---
+++ app/views/product/show.php
@@ -0,0 +1,10 @@
+<h2>Detalles del Producto</h2>
+
+<p><strong>ID:</strong> <?php echo htmlspecialchars($product['id']); ?></p>
+<p><strong>Nombre:</strong> <?php echo htmlspecialchars($product['name']); ?></p>
+<p><strong>Precio:</strong> <?php echo htmlspecialchars($product['price']); ?></p>
+<p><strong>Descripción:</strong> <?php echo htmlspecialchars($product['description']); ?></p>
+<p><strong>Stock:</strong> <?php echo htmlspecialchars($product['stock']); ?></p>
+<p><strong>Categoría:</strong> <?php echo htmlspecialchars($product['category_id']); ?></p>
+
+<a href="/products">Volver a la lista</a>
